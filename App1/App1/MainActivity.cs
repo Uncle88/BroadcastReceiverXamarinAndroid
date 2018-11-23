1e6b72271b0c7279410c325b87c5f10054afc23d
uncle <andrey.proskura@w-src.com>
2018-11-23 16:22:26 +0200

    add animation to main screen

---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Views.Animations;
+using Android.Widget;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SampleReceiver receiver;
+        private Button button;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            Animation anim = AnimationUtils.LoadAnimation(this.BaseContext, Resource.Drawable.move_up);
+            ImageView myImage = FindViewById<ImageView>(Resource.Id.image);
+            myImage.StartAnimation(anim);
+
+            var _broadcastReceiver = new SampleReceiver();
+            _broadcastReceiver.ConnectionStatusChanged += OnNetworkStatusChanged;
+            Application.Context.RegisterReceiver(_broadcastReceiver,
+            new IntentFilter(ConnectivityManager.ConnectivityAction));
+        }
+
+        private void OnNetworkStatusChanged(object sender, EventArgs e)
+        {
+            if (IsOnline())
+            {
+                //Toast.MakeText(this, "Network Activated", ToastLength.Long).Show();
+                CreateDialog();
+            }
+            else
+            {
+                //Toast.MakeText(this, "Network NonActivated", ToastLength.Long).Show();
+                CreateDialog();
+            }
+        }
+
+        private bool IsOnline()
+        {
+            var cm = (ConnectivityManager)GetSystemService(ConnectivityService);
+            return cm.ActiveNetworkInfo == null ? false : cm.ActiveNetworkInfo.IsConnected;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(receiver, new IntentFilter("com.App1.SampleReceiver"));
+        }
+
+        protected override void OnPause()
+        {
+            if (receiver != null)
+            {
+                UnregisterReceiver(receiver);
+            }
+            base.OnPause();
+        }
+
+        private void CreateDialog()
+        {
+            var dialog = new Android.Support.V7.App.AlertDialog.Builder(this);
+            dialog.SetView(Resource.Layout.CheckDialog);
+
+            dialog.SetNeutralButton("Check", (sender, args) =>
+            {
+                StartActivity(new Intent(Android.Provider.Settings.ActionApplicationDetailsSettings,
+                Android.Net.Uri.Parse("package:" + Application.Context.PackageName)));
+            });
+
+            dialog.SetNegativeButton("Cancel", (sender, args) =>
+            {
+                dialog.SetCancelable(true);
+            });
+            dialog.Create();
+            dialog.Show();
+        }
+    }
+}
+
---
+++ App1/App1/SampleReceiver.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using System;
+
+namespace App1
+{
+    [BroadcastReceiver(Name = "com.App1.SampleReceiver", Enabled = true, Exported = true)]
+    [IntentFilter(new[] { Intent.ActionBootCompleted })]
+    public class SampleReceiver : BroadcastReceiver
+    {
+        public event EventHandler ConnectionStatusChanged;
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (ConnectionStatusChanged != null)
+                ConnectionStatusChanged(this, EventArgs.Empty);
+        }
+    }
+}
